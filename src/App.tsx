ad35495ebdf915912eb1801a6c7b3c84a3f7e736
zmarc3ll <zubermarcellpetrik@gmail.com>
2023-02-14 12:24:39 +0100

    listing out data

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Gyumolcs{
+  id: number;
+  nev: string;
+  darab: number;
+}
+
+interface GyomolcsResponse{
+  gyumolcs: Gyumolcs[];
+}
+
+interface State{
+  gyumolcs: Gyumolcs[];
+  nevInput: string;
+  darabInput: number;
+}
+
+
+
+class App extends Component<{}, State>{
+  
+  constructor(props: {}){
+    super(props);
+
+    this.state = {
+      nevInput: '',
+      darabInput: 0,
+      gyumolcs:[]
+    }
+  }
+    async loadGyumolcsok() {
+      let response = await fetch('http://localhost:3001/GyumolcsData');
+      let data = await response.json() as Gyumolcs[];
+      console.log(data);
+      this.setState({
+        gyumolcs: data, 
+      })
+    }
+
+    componentDidMount() {
+      this.loadGyumolcsok();
+    }
+
+    handleUpload = async () => {
+      const { nevInput, darabInput } = this.state;
+      if(nevInput.trim() === '' || darabInput < 1 ){
+        return;
+      }
+
+      const dbData = {
+          nev: nevInput,
+          darab: darabInput
+      }
+
+      let response = await fetch('http://localhost:3001/GyumolcsData', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(dbData),
+    });
+
+    this.setState ({
+      nevInput: '',
+      darabInput:0,
+    });
+
+    await this.loadGyumolcsok();
+    };
+
+    render() {
+      console.log(this.state);
+      const {nevInput, darabInput} = this.state;
+      return <div>
+        <h2>Gyümölcs felvétele:</h2>
+        Név: <input type="text" value={nevInput} onChange={e => this.setState({ nevInput: e.currentTarget.value})} /> <br />
+        Darab: <input type="number" value={darabInput} onChange={e => this.setState({ darabInput: parseInt(e.currentTarget.value) })}/> <br />
+        <button onClick={this.handleUpload}>Hozzáaddás</button> <br />
+        <h2>Már felvett gyümölcsök:</h2>
+      <table>
+        <td>{
+            this.state.gyumolcs.map(gyumolcs => 
+              <tr>{gyumolcs.nev}</tr>
+            )
+          }</td>
+          <td>{
+            this.state.gyumolcs.map(gyumolcs => 
+              <tr>{gyumolcs.darab} db</tr>
+            )
+          }</td>
+          </table>  
+      </div>
+  
+    }
 }
 
 export default App;
